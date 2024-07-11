3f10547df4d552706e60674fdfd418e77ae17228
Junbo Zhang <dr.jimbozhang@gmail.com>
2024-07-11 14:27:00 +0800

    Chenyu's fix (#3)
    * add pretrained model kwargs
    
    * 修改代码避免key值重复
    
    * update
    
    * 更新 pyproject.toml
    
    * Update version
    
    * Update pretrained.py
    
    * Update pretrained.py
    
    * Update pyproject.toml
    
    ---------
    
    Co-authored-by: liuchenyu3 <liuchenyu3@xiaomi.com>

--- dasheng/pretrained/pretrained.py
+++ dasheng/pretrained/pretrained.py
@@ -85,16 +85,25 @@ class Dasheng(AudioTransformerMAE_Encoder):
 
 
 def dasheng_base(**model_kwargs):
+    model_kwargs["embed_dim"] = 768
+    model_kwargs["depth"] = 12
+    model_kwargs["num_heads"] = 12
     return Dasheng.from_pretrained(PRETRAINED_CHECKPOINTS['dasheng_base'],
                                    **model_kwargs)
 
 
 def dasheng_06B(**model_kwargs):
+    model_kwargs["embed_dim"] = 1280
+    model_kwargs["depth"] = 32
+    model_kwargs["num_heads"] = 16
     return Dasheng.from_pretrained(PRETRAINED_CHECKPOINTS['dasheng_06B'],
                                    **model_kwargs)
 
 
 def dasheng_12B(**model_kwargs):
+    model_kwargs["embed_dim"] = 1536
+    model_kwargs["depth"] = 40
+    model_kwargs["num_heads"] = 24
     return Dasheng.from_pretrained(PRETRAINED_CHECKPOINTS['dasheng_12B'],
                                    **model_kwargs)
 
